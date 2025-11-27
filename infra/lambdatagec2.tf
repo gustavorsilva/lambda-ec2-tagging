da11dec6409eabeb9df9123b9806bc3d74f80278
Gustavo Rodrigues <gustavo_rodriguees@hotmail.com>
2025-11-26 21:18:19 -0300

    Update lambdatagec2.tf

--- infra/lambdatagec2.tf
+++ infra/lambdatagec2.tf
@@ -1,38 +1,42 @@
-import boto3
-import fnmatch
+#########
+# LAMBDA
+#########
 
-region = 'us-east-1'
+resource "aws_lambda_function" "lambda" {
+  filename      = "lambda_function_payload.zip"
+  function_name = "stopec2"
+  role          = "arn:aws:iam::119739005737:role/startandstop"
+  handler       = "lambda_function.lambda_handler"
+  runtime       = "python3.9"
+  publish       = true
+  timeout       = 30
+  memory_size   = 128
+}
 
-def lambda_handler(event, context):
-    # Obtém o nome da instância EC2 do evento
-    instance_names = ['ss*','neg*','nod*']
-    
-    # Define as tags a serem inseridas nas instâncias
-    tags = [
-        {'Key': 'env', 'Value': 'dev'},
-        {'Key': 'Project', 'Value': 'Test'}
-    ]
-    
-    # Cria uma instância do cliente EC2
-    ec2 = boto3.client('ec2')
-    
-    # Obtém todas as instâncias EC2
-    response = ec2.describe_instances()
-    instances = response['Reservations']
-    
-    # Percorre as instâncias EC2 e insere as tags nas correspondentes aos nomes fornecidos
-    for reservation in instances:
-        for instance in reservation['Instances']:
-            if 'Tags' in instance:
-                for tag in instance['Tags']:
-                    if tag['Key'] == 'Name':
-                        for pattern in instance_names:
-                            if fnmatch.fnmatch(tag['Value'], pattern):
-                                instance_id = instance['InstanceId']
-                                ec2.create_tags(Resources=[instance_id], Tags=tags)
-    
-    # Retorna uma resposta de sucesso
-    return {
-        'statusCode': 200,
-        'body': 'Tags inseridas com sucesso nas instâncias EC2.'
-    }
+###########
+# scheduler
+###########
+
+resource "aws_cloudwatch_event_rule" "profile_generator_stop_ec2" {
+  name = "profile-generator-stop-event-rule"
+  description = "Stop"
+  schedule_expression = "cron(15 07 ? * * *)"
+}
+
+resource "aws_cloudwatch_event_target" "profile_generator_stop_target" {
+  arn = "arn:aws:lambda:us-east-1:119739005737:function:stopec2"
+  rule = aws_cloudwatch_event_rule.profile_generator_stop_ec2.name
+  input = <<JSON
+{
+  "env": "dev",
+  "action": "Stop"
+} 
+  JSON
+}
+
+resource "aws_lambda_permission" "trigger_scheduller" {
+    action = "lambda:InvokeFunction"
+    function_name = aws_lambda_function.lambda.function_name
+    principal = "events.amazonaws.com"
+    source_arn = "${aws_cloudwatch_event_rule.profile_generator_stop_ec2.arn}"
+}
